a2a4e59a07a2d42fcdca346d1c2f7a3b92d3589a
yosrimhamdi <yosrimhamdi@outlook.com>
2021-08-23 15:01:34 +0100

    added a webpack alias.

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,7 +1,13 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
-// import fetchPosts from '../redux/actions/'
+import fetchPosts from '@actions/fetchPosts';
 
-const PostList = () => <div>postList</div>;
+const PostList = () => {
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+
+  return <div>postList</div>;
+};
 
 export default PostList;
